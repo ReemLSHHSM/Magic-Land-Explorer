a8204fc7cf0e2b7d34fe5a8e3a1369ead937f79d
Reem Hasan <23039864@student.ltuc.com>
2024-07-21 13:41:04 +0300

    Add project files.

---
+++ MagicLandExplorer/Category.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicLandExplorer
+{
+    
+    public class Category
+    {
+        List<Destination> destinations = new List<Destination>();
+
+        public string CategoryName { get; set; }
+
+
+        public Category(string CategoryName)
+        {
+            this.CategoryName = CategoryName;
+        }
+
+
+
+
+    }
+}
---
+++ MagicLandExplorer/Destination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicLandExplorer
+{
+    internal class Destination
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Description { get; set; }
+
+        public Destination(string name, string type, string location, TimeSpan duration, string description)
+        {
+            Name = name;
+            Type = type;
+            Location = location;
+            Duration = duration;
+            Description = description;
+        }
+    }
+}
